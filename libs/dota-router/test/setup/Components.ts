fa767c6516a01ac1606d3bce6304b593f839488e
ayu-sh-kr <akjaiswal2003@gmail.com>
2025-10-21 14:01:22 +0530

    Add test components for routing setup
    Introduces several test components with route decorators for use in dota-router tests, including Home, Products, Doc, Resource, About, Contact, and Dummy components.

---
+++ libs/dota-router/test/setup/Components.ts
@@ -0,0 +1,104 @@
+import {BaseElement, Component} from "@ayu-sh-kr/dota-core";
+import { Route } from "@dota/route.decorator";
+
+@Route({
+  path: '/',
+})
+@Component({selector: 'dota-home'})
+export class HomeComponent extends BaseElement {
+
+  constructor() {
+    super();
+  }
+
+  render(): string {
+    return '<div>Home</div>';
+  }
+}
+
+@Route({
+  path: '/shop/product',
+})
+@Component({selector: 'dota-products'})
+export class ProductsComponent extends BaseElement {
+
+  constructor() {
+    super();
+  }
+
+  render(): string {
+    return '<div>Products</div>';
+  }
+}
+
+@Route({
+  path: '/doc',
+})
+@Component({selector: 'dota-doc'})
+export class DocComponent extends BaseElement {
+
+  constructor() {
+    super();
+  }
+
+  render(): string {
+    return '<div>Doc</div>';
+  }
+}
+
+@Route({
+  path: '/resource',
+})
+@Component({selector: 'dota-resource'})
+export class ResourceComponent extends BaseElement {
+
+  constructor() {
+    super();
+  }
+
+  render(): string {
+    return '<div>Resource</div>';
+  }
+}
+
+@Route({
+  path: '/resource/about',
+})
+@Component({selector: 'dota-about'})
+export class AboutComponent extends BaseElement {
+
+  constructor() {
+    super();
+  }
+
+  render(): string {
+    return '<div>About</div>';
+  }
+}
+
+@Route({
+  path: '/resource/contact',
+})
+@Component({selector: 'dota-contact'})
+export class ContactComponent extends BaseElement {
+
+  constructor() {
+    super();
+  }
+
+  render(): string {
+    return '<div>Contact</div>';
+  }
+}
+
+@Component({selector: 'dota-dummy'})
+export class DummyComponent extends BaseElement {
+
+  constructor() {
+    super();
+  }
+
+  render(): string {
+    return '<div>Dummy</div>';
+  }
+}
