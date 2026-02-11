6146fb737c530c553f46e07d2601c2079ff2788f
ayu-sh-kr <akjaiswal2003@gmail.com>
2026-02-11 13:48:23 +0530

    Add virtual module types for dota components
    Add a TypeScript declaration file declaring module "virtual:dota-components" that exports a default array of DotaElementConstructor imported from "@ayu-sh-kr/dota-core". This lets the dota-web app and TypeScript recognize imports from the virtual module providing component constructors.

---
+++ packages/apps/dota-web/src/virtual-modules.d.ts
@@ -0,0 +1,6 @@
+
+declare module "virtual:dota-components" {
+  import {DotaElementConstructor} from "@ayu-sh-kr/dota-core";
+  const components: DotaElementConstructor[];
+  export default components;
+}
