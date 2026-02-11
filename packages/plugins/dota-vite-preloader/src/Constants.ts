dd2ae42ddd53377d6ea160b7b75aca24ae45e720
ayu-sh-kr <akjaiswal2003@gmail.com>
2026-02-11 13:49:11 +0530

    Add AST constants and rename virtual import ID
    Introduce ASTFilterConstants to centralize AST-related identifiers (Component decorator name, render method name, selector property). Rename VirtualModuleId to VirtualImportID and add RESOLVED_DOTA_COMPONENTS (\0-prefixed) to represent the resolved virtual module ID used by Vite. These changes clarify naming and provide a resolved virtual import identifier for plugin usage.

--- packages/plugins/dota-vite-preloader/src/Constants.ts
+++ packages/plugins/dota-vite-preloader/src/Constants.ts
@@ -6,10 +6,17 @@ export class ComponentScanPath {
   static SOURCE_ROOT_DIRECTORY_SCAN_PATH = "./src/**/*.component.ts";
 }
 
+export class ASTFilterConstants {
+  static COMPONENT_DECORATOR_NAME = 'Component';
+  static CLASS_RENDER_METHOD_NAME = 'render';
+  static COMPONENT_TAG_NAME_PROPERTY = 'selector';
+}
+
 export class ImportPath {
   static DOTA_CORE = "@ayu-sh-kr/dota-core";
 }
 
-export class VirtualModuleId {
+export class VirtualImportID {
   static DOTA_COMPONENTS = "virtual:dota-components";
+  static RESOLVED_DOTA_COMPONENTS = "\0virtual:dota-components";
 }
