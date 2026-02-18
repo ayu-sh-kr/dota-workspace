8463edd05eb402d56edf296067911ac08043fc00
ayu-sh-kr <akjaiswal2003@gmail.com>
2026-02-18 22:22:06 +0530

    Publish removal event for notifications
    Notification component now publishes a 'notification:removed' event via ApplicationEventService (including type, message, color, icon, title) before removing the node, allowing other parts of the app to react to dismissed notifications. Also cleaned up minor code: removed an unused HTML import from AppComponent and deleted an unused notificationHolderId field from NotificationService; whitespace/formatting adjusted.

--- packages/apps/dota-web/src/app.component.ts
+++ packages/apps/dota-web/src/app.component.ts
@@ -1,4 +1,4 @@
-import {BaseElement, Component, HTML} from "@ayu-sh-kr/dota-core";
+import {BaseElement, Component} from "@ayu-sh-kr/dota-core";
 import '@dota/pages/home.page.ts'
 import '@dota/pages/doc.page.ts'
 import '@dota/pages/error.page.ts'
@@ -7,23 +7,23 @@ import {GeneralUtils} from "@dota/utils/GeneralUtils.ts";
 
 
 @Component({
-    selector: 'app-root',
-    shadow: false
+  selector: 'app-root',
+  shadow: false
 })
 export class AppComponent extends BaseElement {
 
-    constructor() {
-        super();
-    }
+  constructor() {
+    super();
+  }
 
-    @AfterInit()
-    afterViewInit() {
-        const browserTheme = GeneralUtils.getBrowserTheme();
-        GeneralUtils.setBrowserTheme(browserTheme);
-    }
+  @AfterInit()
+  afterViewInit() {
+    const browserTheme = GeneralUtils.getBrowserTheme();
+    GeneralUtils.setBrowserTheme(browserTheme);
+  }
 
-    render(): string {
-        return ''
-    }
+  render(): string {
+    return ''
+  }
 
 }
