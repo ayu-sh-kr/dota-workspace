cfbafdd4ef6110b07fcc350376a82fe967ccb2f9
ayu-sh-kr <akjaiswal2003@gmail.com>
2026-02-18 22:20:48 +0530

    Add entrypoint exports for dota-event
    Create packages/libs/dota-event/src/main.ts as the package entrypoint and re-export core types and default event classes. Exports include @dota/Types, DefaultApplicationEventManager, DefaultApplicationEventBus, DefaultApplicationEventPublisher, DefaultApplicationEventListener, and the on-event decorator to simplify imports for consumers.

---
+++ packages/libs/dota-event/src/main.ts
@@ -0,0 +1,7 @@
+export * from '@dota/Types';
+export {DefaultApplicationEventManager} from '@dota/DefaultApplicationEventManager.ts';
+export {DefaultApplicationEventBus} from '@dota/DefaultApplicationEventBus.ts';
+export {DefaultApplicationEventPublisher} from '@dota/DefaultApplicationEventPublisher.ts';
+export {DefaultApplicationEventListener} from '@dota/DefaultApplicationEventListener.ts';
+export * from "@dota/on-event.decorator.ts"
+
