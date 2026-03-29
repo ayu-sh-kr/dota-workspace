e8fb02a7ea7b2366a82ef51ea714c8bc5c7b0e55
ayu-sh-kr <akjaiswal2003@gmail.com>
2026-03-29 12:11:46 +0530

    Add query contracts and impl exports
    Introduce barrel files for the query module in dota-ast-utils: add contracts/index.ts to re-export query types (DeclarationTerminalQuery, DeclarationBaseQuery, DeclarationFluentQuery variants), impl/index.ts to export implementation classes (DeclarationTerminalQueryImpl, ClassDeclarationQueryImpl, FunctionDeclarationQueryImpl, VariableDeclarationQueryImpl, ExportDeclarationQueryImpl, ExportNamedDeclarationQueryImpl), and query/index.ts to aggregate both. This centralizes and simplifies imports for query types and implementations.

---
+++ packages/utils/dota-ast-utils/src/query/contracts/index.ts
@@ -0,0 +1,10 @@
+export type {DeclarationTerminalQuery} from "./DeclarationTerminalQuery";
+export type {DeclarationBaseQuery} from "./DeclarationBaseQuery";
+export type {
+  DeclarationFluentQuery,
+  ExportNamedDeclarationQuery,
+  VariableDeclarationQuery,
+  ClassDeclarationQuery,
+  FunctionDeclarationQuery,
+  ExportDeclarationQuery,
+} from "./DeclarationFluentQuery";
---
+++ packages/utils/dota-ast-utils/src/query/impl/index.ts
@@ -0,0 +1,6 @@
+export {DeclarationTerminalQueryImpl} from "./DeclarationTerminalQueryImpl";
+export {ClassDeclarationQueryImpl} from "./ClassDeclarationQueryImpl";
+export {FunctionDeclarationQueryImpl} from "./FunctionDeclarationQueryImpl";
+export {VariableDeclarationQueryImpl} from "./VariableDeclarationQueryImpl";
+export {ExportDeclarationQueryImpl} from "./ExportDeclarationQueryImpl";
+export {ExportNamedDeclarationQueryImpl} from "./ExportNamedDeclarationQueryImpl";
