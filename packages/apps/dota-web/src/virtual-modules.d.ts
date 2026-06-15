607861f4f05e05c41373235bddd4198d24d45285
ayu-sh-kr <akjaiswal2003@gmail.com>
2026-06-15 17:48:10 +0530

    Add virtual:dota-routes type declaration
    Add a TypeScript declaration for "virtual:dota-routes" in packages/apps/dota-web/src/virtual-modules.d.ts. The new module imports RouteConfig from @ayu-sh-kr/dota-router and exports routeConfig as RouteConfig<HTMLElement>[]. Also add a trailing newline to the file.

--- packages/apps/dota-web/src/virtual-modules.d.ts
+++ packages/apps/dota-web/src/virtual-modules.d.ts
@@ -5,6 +5,10 @@ declare module "virtual:dota-components" {
   export default components;
 }
 
+declare module "virtual:dota-routes" {
+  import {RouteConfig} from "@ayu-sh-kr/dota-router";
+  export const routeConfig: RouteConfig<HTMLElement>[];
+}
 
 declare module "virtual:dota-component-modules" {
   const modules: Record<string, any>;
