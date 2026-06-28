24ca0827312052a07b1f22880cb4f23de397cab2
ayu-sh-kr <akjaiswal2003@gmail.com>
2026-06-28 11:51:58 +0530

    Switch virtual module imports to dota-wrap
    Update type import paths in packages/apps/dota-web/src/virtual-modules.d.ts to use the reorganized @ayu-sh-kr/dota-wrap packages (core and router). This aligns the virtual module declarations with the package restructuring so DotaElementConstructor and RouteConfig types resolve correctly.

--- packages/apps/dota-web/src/virtual-modules.d.ts
+++ packages/apps/dota-web/src/virtual-modules.d.ts
@@ -1,12 +1,12 @@
 
 declare module "virtual:dota-components" {
-  import {DotaElementConstructor} from "@ayu-sh-kr/dota-core";
+  import {DotaElementConstructor} from "@ayu-sh-kr/dota-wrap/core";
   const components: DotaElementConstructor[];
   export default components;
 }
 
 declare module "virtual:dota-routes" {
-  import {RouteConfig} from "@ayu-sh-kr/dota-router";
+  import {RouteConfig} from "@ayu-sh-kr/dota-wrap/router";
   export const routeConfig: RouteConfig<HTMLElement>[];
 }
 
