e6f23494942527dc7a420f2492d1f3e2e4b547e4
Ayush Jaiswal <akjaiswal2003@gmail.com>
2026-08-08 18:11:24 +0530

    Add type exports for `RouteRenderErrorPolicy` and `RouteRendererOptions`
    - Exported types `RouteRenderErrorPolicy` and `RouteRendererOptions` from `@dota/coordinator/route-renderer` module
    - Provides improved type support for consumers using these options in route rendering operations

--- packages/libs/dota-router/src/coordinator/index.ts
+++ packages/libs/dota-router/src/coordinator/index.ts
@@ -1,6 +1,7 @@
 export {HistoryCoordinator} from "@dota/coordinator/HistoryCoordinator";
 export {NavigationCoordinator} from "@dota/coordinator/NavigationCoordinator";
 export {createRouteRenderer, renderRoute} from "@dota/coordinator/route-renderer";
+export type {RouteRenderErrorPolicy, RouteRendererOptions} from "@dota/coordinator/route-renderer";
 export {createNavigationContext, getBranchDelta, toNavigationResult, toNavigationUrl} from "@dota/coordinator/route-transition";
 export {runRouteGuards, runRouteLifecycleHooks} from "@dota/coordinator/navigation-lifecycle";
 export type {Coordinator} from "@dota/coordinator/Coordinator";
